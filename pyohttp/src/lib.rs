7d449c8cb57e43f0fc55a54f0ea60e40c6ea2177
Kapil Vaswani <kapilv@microsoft.com>
2024-11-19 16:53:15 +0000

    async support

--- pyohttp/src/lib.rs
+++ pyohttp/src/lib.rs
@@ -1,45 +1,56 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+use core::str;
 use ohttp_client::OhttpClientBuilder;
 use pyo3::prelude::*;
 use reqwest::Response;
-use core::str;
-use std::{collections::HashMap, path::PathBuf, string::String};
+use std::{collections::HashMap, path::PathBuf, string::String, sync::Arc};
+use tokio::sync::Mutex;
 
 #[pyclass]
 struct OhttpResponse {
-    response: Response,
+    response: Arc<Mutex<Response>>,
 }
 
 #[pymethods]
 impl OhttpResponse {
     fn status(&self) -> u16 {
-        self.response.status().as_u16()
+        let response = Arc::clone(&self.response);
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let f = async move { response.lock().await.status().as_u16() };
+        rt.block_on(f)
     }
 
     fn headers(&self) -> HashMap<String, String> {
-        self.response
-            .headers()
-            .iter()
-            .filter_map(|(key, value)| {
-                value
-                    .to_str()
-                    .ok()
-                    .map(|value_str| (key.as_str().to_string(), value_str.to_string()))
-            })
-            .collect()
+        let response = Arc::clone(&self.response);
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let f = async move {
+            response
+                .lock()
+                .await
+                .headers()
+                .iter()
+                .filter_map(|(key, value)| {
+                    value
+                        .to_str()
+                        .ok()
+                        .map(|value_str| (key.as_str().to_string(), value_str.to_string()))
+                })
+                .collect::<HashMap<String, String>>()
+        };
+        rt.block_on(f)
     }
 
-    fn chunk(&mut self) -> Option<Vec<u8>> {
-        let f = async {
-            match self.response.chunk().await {
+    fn chunk<'py>(&mut self, py: Python<'py>) -> PyResult<&'py PyAny> {
+        let response = Arc::clone(&self.response);
+        pyo3_asyncio::tokio::future_into_py(py, async move {
+            let chunk = match response.lock().await.chunk().await {
                 Ok(Some(chunk)) => Some(chunk.to_vec()),
                 _ => None,
-            }
-        };
-        let rt = tokio::runtime::Runtime::new().unwrap();
-        rt.block_on(f)
+            };
+            Ok(chunk)
+        })
     }
 }
 
@@ -56,56 +67,60 @@ impl OhttpClient {
         OhttpClient { kms_url, kms_cert }
     }
 
-    pub fn post(
+    pub fn post<'py>(
         &self,
         url: String,
-        binary: bool,
-        target_path: String,
         headers: HashMap<String, String>,
         form_fields: HashMap<String, String>,
         outer_headers: HashMap<String, String>,
-    ) -> PyResult<OhttpResponse> {
-        let f = async {
-            let headers = headers
-                .iter()
-                .map(|(key, value)| format!("{}: {}", key, value))
-                .collect();
-            let form_fields = form_fields
-                .iter()
-                .map(|(key, value)| format!("{}={}", key, value))
-                .collect();
-            let outer_headers = outer_headers
-                .iter()
-                .map(|(key, value)| format!("{}: {}", key, value))
-                .collect();
+        py: Python<'py>,
+    ) -> PyResult<&'py PyAny> {
+        let kms_url = self.kms_url.clone();
+        let kms_cert = self.kms_cert.clone();
+        let headers = headers
+            .iter()
+            .map(|(key, value)| format!("{}: {}", key, value))
+            .collect();
+        let form_fields = form_fields
+            .iter()
+            .map(|(key, value)| format!("{}={}", key, value))
+            .collect();
+        let outer_headers = outer_headers
+            .iter()
+            .map(|(key, value)| format!("{}: {}", key, value))
+            .collect();
 
+        pyo3_asyncio::tokio::future_into_py(py, async move {
             let client = OhttpClientBuilder::new()
-                .kms_url(&Some(self.kms_url.clone()))
-                .kms_cert(&Some(self.kms_cert.clone()))
+                .kms_url(&Some(kms_url.clone()))
+                .kms_cert(&Some(kms_cert.clone()))
                 .build()
-                .await?;
+                .await
+                .map_err(|e: Box<dyn std::error::Error>| {
+                    PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e))
+                })?;
 
             let response = client
                 .post(
                     &url,
-                    binary,
-                    &target_path,
+                    false,
+                    &"/",
                     &headers,
                     &form_fields,
                     &outer_headers,
                 )
-                .await?;
+                .await
+                .map_err(|e: Box<dyn std::error::Error>| {
+                    PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e))
+                })?;
 
-            Ok(OhttpResponse { response })
-        };
-        let rt = tokio::runtime::Runtime::new().unwrap();
-        rt.block_on(f).map_err(|e: Box<dyn std::error::Error>| {
-            PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e))
+            Ok(OhttpResponse {
+                response: Arc::new(Mutex::new(response)),
+            })
         })
     }
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn pyohttp(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<OhttpResponse>()?;
